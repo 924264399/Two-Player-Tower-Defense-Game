64b8d88881c22ddff4aefa35f716e49bff429657
刘耀宇 <924264399@qq.com>
2025-09-23 07:17:26 +0800

    0717

---
+++ doublePlayersGames/manager.h
@@ -0,0 +1,31 @@
+#ifndef  _MANAGER_H_
+#define _MANAGER_H
+
+
+class Manage
+{
+
+public:
+	
+	
+	~Manage() = default;
+
+
+
+private:
+
+	Manage() = default; //构造函数私有 无法创建实例化对象
+
+
+
+
+};
+
+
+
+
+
+
+
+
+#endif // ! _MANAGER_H_
