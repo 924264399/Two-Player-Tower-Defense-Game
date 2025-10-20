1f1eca3f53fcb8a68c19aca2ab78006abc97a479
刘耀宇 <924264399@qq.com>
2025-10-21 07:09:14 +0800

    1021

--- doublePlayersGames/config_manager.h
+++ doublePlayersGames/config_manager.h
@@ -142,10 +142,72 @@ public:
 			wave_list.emplace_back();
 			Wave& wave = wave_list.back();
 
+			cJSON* json_wave_rewards = cJSON_GetObjectItem(json_wave, "rewards");   //rawards是wave的属性
+			if (json_wave_rewards && json_wave_rewards->type == cJSON_Number)		//检查
+				wave.rawards = json_wave_rewards->valuedouble;
+
+
+			cJSON* json_wave_interval = cJSON_GetObjectItem(json_wave, "interval");   //interval也是是wave的属性
+			if (json_wave_interval && json_wave_interval->type == cJSON_Number)
+				wave.interval = json_wave_interval->valuedouble;
+
+
+			cJSON* json_wave_spawn_list = cJSON_GetObjectItem(json_wave, "spawn_list"); //spawn_list这个属性是个数组 所有获取到了之后还需要进行遍历
+			if (json_wave_spawn_list && json_wave_spawn_list->type == cJSON_Array)
+			{
+				cJSON* json_spawn_event = nullptr;
+				cJSON_ArrayForEach(json_spawn_event, json_wave_spawn_list)   //遍历json_wave_spawn_list  存储到json_spawn_event
+				{
+					if (json_spawn_event->type != cJSON_Object)
+						continue;
+
+
+					wave.spawn_event_list.emplace_back();
+					Wave::SpawnEvent& spawn_event = wave.spawn_event_list.back();
+
+					cJSON* json_spawn_event_interval = cJSON_GetObjectItem(json_spawn_event, "interval"); //spawn_list 这个数组的结构是每个元素都有interval(double) point(int) enemy(enemy比较麻烦 是个字典一样的结果 有key 和value)
+					if (json_spawn_event_interval && json_spawn_event_interval->type == cJSON_Number)
+						spawn_event.interval = json_spawn_event_interval->valuedouble;
+
+
+					cJSON* json_spawn_event_spawn_point = cJSON_GetObjectItem(json_spawn_event, "point");
+					if (json_spawn_event_spawn_point && json_spawn_event_spawn_point->type == cJSON_Number)
+						spawn_event.spawn_point = json_spawn_event_spawn_point->valueint;
+
+
+					cJSON* json_spawn_event_enemy_type = cJSON_GetObjectItem(json_spawn_event, "enemy");
+					if (json_spawn_event_enemy_type && json_spawn_event_enemy_type->type == cJSON_String)
+					{
+						const std::string str_emery_type = json_spawn_event_enemy_type->valuestring;     //valuestring是这个字典结构的 value的数据类型
+
+						if (str_emery_type == "Slim")
+							spawn_event.enemy_type = EnemyType::Slim;
+						else if (str_emery_type == "KingSlim")
+							spawn_event.enemy_type = EnemyType::KingSlim;
+						else if (str_emery_type == "Skeleton")
+							spawn_event.enemy_type = EnemyType::Skeleton;
+						else if (str_emery_type == "Goblin")
+							spawn_event.enemy_type = EnemyType::Goblin;
+						else if (str_emery_type == "GoblinPriest")
+							spawn_event.enemy_type = EnemyType::GoblinPriest;
+					
+
+					}
+
+				}
+
+				if (wave.spawn_event_list.empty())
+					wave_list.pop_back();    //这是为了防止 第三个属性spawn_list数组 都是空的 导致整个最后填充的不合法
+			}
 
 		}
 
+		cJSON_Delete(json_root);  //释放一下
 
+		if (wave_list.empty())
+			return false;          //如果这都是空的  那说明文件有文件 直接false
+
+		return true;
 	}
 
 
