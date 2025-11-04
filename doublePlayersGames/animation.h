2fb05bd90c6a0c7822d6878f222ee608cab32106
刘耀宇 <924264399@qq.com>
2025-11-05 07:37:25 +0800

    1105

--- doublePlayersGames/animation.h
+++ doublePlayersGames/animation.h
@@ -78,6 +78,45 @@ public:
 	}
 
 
+	//是否要循环播放
+	void set_loop(bool is_loop)
+	{
+		this->is_loop = is_loop;
+	}
+
+
+	//设置帧间隔
+	void set_interval(double interval)
+	{
+		timer.set_wait_time(interval);
+	}
+
+	//设置回调函数
+	void set_on_finished(PlayCallback on_finished)
+	{
+		this->on_finished = on_finished;
+	}
+
+	//定时器更新
+	void on_update(double delta)
+	{
+		timer.on_update(delta);
+	}
+
+
+
+	///核心 渲染的  输入渲染的位置  有的序列动画可能还需要旋转 所以这里给了一个angle角度的输入
+	void on_render(SDL_Renderer* renderer, const SDL_Point& pos_dst, double angle = 0) const
+	{
+		static SDL_Rect rect_dst;
+
+		rect_dst.x = pos_dst.x, rect_dst.y = pos_dst.y;
+		rect_dst.w = width_frame, rect_dst.h = height_frame;
+
+		SDL_RenderCopyEx(renderer, texture, &rect_src_list[idx_frame], &rect_dst, angle, nullptr, SDL_RendererFlip::SDL_FLIP_NONE);  //第三个是裁剪矩形  rect_dst目标矩形  SDL_FLIP_NONE不进行翻转 
+	}
+
+
 
 
 
